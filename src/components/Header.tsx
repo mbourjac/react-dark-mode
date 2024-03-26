f8be92a92f3fb5032b08d948c68a1d7f15fe34c3
Michaël Bourjac <michael.bourjac@gmail.com>
2024-03-26 08:31:59 +0100

    feat: improve hero responsive layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ export const Header = () => {
   const { toggleDarkMode } = useDarkMode();
 
   return (
-    <header className="sticky top-4">
-      <div className="flex items-center justify-between gap-8 border-4 border-r-0 border-black bg-white px-2 text-4xl font-semibold uppercase transition-all dark:border-white dark:bg-black dark:text-white">
+    <header className="sticky top-4 text-2xl font-semibold uppercase md:text-3xl lg:text-4xl dark:text-white">
+      <div className="flex items-center justify-between gap-8 border-4 border-r-0 border-black bg-white px-2 transition-all dark:border-white dark:bg-black">
         <h1>Lorem ipsum</h1>
         <div className="flex items-center gap-10">
-          <nav className="flex gap-8">
+          <nav className="hidden gap-8 sm:flex">
             <Link to="/">Fugit</Link>
             <Link to="/">Voluptatem</Link>
           </nav>
--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -3,10 +3,10 @@ import hero from '../../assets/images/hero.jpeg';
 export const Hero = () => {
   return (
     <section className="font-bold">
-      <h2 className="br-border p-2 text-8xl uppercase">
+      <h2 className="br-border p-2 text-2xl uppercase leading-none sm:text-[min(5.5vw,8rem)]">
         Dolor sit amet consectetur
       </h2>
-      <p className="bl-border p-2 text-2xl">
+      <p className="bl-border p-2 text-xl sm:text-2xl">
         Adipisicing elit, quod dolores temporibus vitae similique fuga, odit
         enim delectus velit nemo ducimus doloribus cumque at asperiores
         deleniti, consequatur suscipit animi explicabo labore.
